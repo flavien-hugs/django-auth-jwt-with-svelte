fa3e4ab8161cee65b4189a5ca3b00d4b548edf47
flavien-hugs <geekactux@gmail.com>
2022-06-22 18:54:52 +0000

    edit project structure

--- frontend/.svelte-kit/generated/client-manifest.js
+++ frontend/.svelte-kit/generated/client-manifest.js
@@ -1,11 +1,17 @@
 export { matchers } from './client-matchers.js';
 
 export const components = [
-	() => import("../runtime/components/layout.svelte"),
+	() => import("../../src/routes/__layout.svelte"),
 	() => import("../runtime/components/error.svelte"),
+	() => import("../../src/routes/accounts/login/index.svelte"),
+	() => import("../../src/routes/accounts/register/index.svelte"),
+	() => import("../../src/routes/accounts/user/[id].svelte"),
 	() => import("../../src/routes/index.svelte")
 ];
 
 export const dictionary = {
-	"": [[0, 2], [1]]
+	"": [[0, 5], [1]],
+	"accounts/login": [[0, 2], [1]],
+	"accounts/register": [[0, 3], [1]],
+	"accounts/user/[id]": [[0, 4], [1]]
 };
