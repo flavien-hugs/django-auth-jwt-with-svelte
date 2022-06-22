f2dff8fc3ee5087aa7979a0016c5402721203c57
flavien-hugs <geekactux@gmail.com>
2022-06-22 18:17:24 +0000

    edit api render to json format

---
+++ backend/accounts/renderers.py
@@ -0,0 +1,29 @@
+# accounts.renderers.py
+
+
+import json
+from typing import Any, Mapping, Optional
+
+from rest_framework.renderers import JSONRenderer
+
+
+class UserJSONRenderer(JSONRenderer):
+
+    charset = 'utf-8'
+
+    def render(
+        self,
+        data: dict[str, Any],
+        media_type: Optional[str] = None,
+        renderer_context: Optional[Mapping[str, Any]] = None,
+    ) -> str:
+
+        errors = data.get('errors', None)
+        token = data.get('token', None)
+        if errors is not None:
+            return super(UserJSONRenderer, self).render(data)
+
+        if token is not None and isinstance(token, bytes):
+            data['token'] = token.decode('utf-8')
+
+        return json.dumps({'user': data})
