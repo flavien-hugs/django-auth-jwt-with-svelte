f9b3662b716c561395b29c3d99a8420c4a34a13f
flavien-hugs <geekactux@gmail.com>
2022-06-24 04:26:13 +0000

    edit component header

---
+++ frontend/src/components/Header/Header.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    import { page } from '$app/stores'
+    import { userData } from '$lib/store/userStore';
+	import { logOutUser } from '$lib/utils/requestUtils';
+</script>
+
+
+<header>
+    <div class="corner">
+        <a href="https://kit.svelte.dev">
+            SvelteKit
+        </a>
+    </div>
+
+    <nav>
+        <svg viewBox="0 0 2 3" aria-hidden="true">
+            <path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
+        </svg>
+        <ul>
+            <li class:active={$page.url.pathname === '/'}>
+                <a sveltekit:prefetch href="/">Home</a>
+            </li>
+            {#if !$userData.username}
+                <li class:active={$page.url.pathname === '/accounts/login'}>
+                    <a sveltekit:prefetch href="/accounts/login">Login</a>
+                </li>
+                <li class:active={$page.url.pathname === '/accounts/register'}>
+                    <a sveltekit:prefetch href="/accounts/register">Register</a>
+                </li>
+            {:else}
+                <li>
+                    Welcome, <a sveltekit:prefetch href="/accounts/user/">{$userData.username}</a>
+                </li>
+                <li>
+                    <a href={null} on:click={logOutUser} style="cursor: pointer;">Logout</a>
+                </li>
+            {/if}
+        </ul>
+        <svg viewBox="0 0 2 3" aria-hidden="true">
+            <path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
+        </svg>
+    </nav>
+
+    <div class="corner">
+        <a href="https://github.com/flavien-hugs/">
+            Flavien HUGS
+        </a>
+    </div>
+</header>
---
+++ frontend/src/components/Loader/Loader.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { loading } from '$lib/store/loadingStore';
+
+	$: if ($loading.status === 'NAVIGATING') {
+		setTimeout(() => {
+			if ($loading.status === 'NAVIGATING') {
+				$loading.status = 'LOADING';
+			}
+		}, 100);
+	}
+</script>
+
+{#if $loading.status === 'LOADING'}
+	<div class="loader-container">
+		<div class="loader" />
+		{#if $loading.message}
+			<p>{$loading.message}</p>
+		{/if}
+	</div>
+{/if}
