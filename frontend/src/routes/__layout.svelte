6f99427e9f658d103f9835736084254d546becae
flavien-hugs <geekactux@gmail.com>
2022-06-24 04:25:18 +0000

    edit routes

---
+++ frontend/src/routes/__layout.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+	import { userData } from '$lib/store/userStore';
+	import { navigating } from '$app/stores';
+	import { loading } from '$lib/store/loadingStore';
+	import { notificationData } from '$lib/store/notificationStore';
+	import { fly } from 'svelte/transition';
+	import { afterUpdate, onMount } from 'svelte';
+
+	import Header from '../components/Header/Header.svelte';
+	import Loader from '../components/Loader/Loader.svelte';
+
+	$: loading.setNavigate(!!$navigating);
+	$: loading.setLoading(!!$navigating, 'Loading, please wait...');
+
+	import { getCurrentUser, browserGet } from '$lib/utils/requestUtils';
+	import { variables } from '$lib/utils/constants';
+
+	onMount(async () => {
+		if (browserGet('refreshToken')) {
+			const [response, errs] = await getCurrentUser(
+				fetch,
+				`${variables.BASE_API_URI}/token/refresh/`,
+				`${variables.BASE_API_URI}/user/`
+			);
+			if (errs.length <= 0) {
+				userData.set(response);
+			}
+		}
+	});
+
+	afterUpdate(async () => {
+		const notifyEl = document.getElementById('notification') as HTMLElement;
+		if (notifyEl && $notificationData !== '') {
+			setTimeout(() => {
+				notifyEl.classList.add('disappear');
+				notificationData.set('');
+			}, 3000);
+		}
+		if (browserGet('refreshToken')) {
+			const [response, _] = await getCurrentUser(
+				fetch,
+				`${variables.BASE_API_URI}/token/refresh/`,
+				`${variables.BASE_API_URI}/user/`
+			);
+			userData.update(() => response);
+		}
+	});
+</script>
+
+<Header />
+
+{#if $notificationData}
+	<p
+		class="notification"
+		id="notification"
+		in:fly={{ x: 200, duration: 500, delay: 500 }}
+		out:fly={{ x: 200, duration: 500 }}
+	>
+		{$notificationData}
+	</p>
+{/if}
+
+<main>
+	<Loader />
+	<slot />
+</main>
+
+<footer in:fly={{ y: -50, duration: 500, delay: 500 }} out:fly={{ duration: 500 }}>
+    <p>
+        Learn SvelteKit. Coded by
+        <a href="https://github.com/flavien-hugs/">
+            Flavien HUGS
+        </a>
+    </p>
+</footer>
--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { userData } from '$lib/store/userStore';
+
+	import { fly } from 'svelte/transition';
+</script>
+
+<svelte:head>
+	<title>Home | FullStack Django & SvelteKit</title>
+</svelte:head>
+
+<section in:fly={{ y: -100, duration: 500, delay: 500 }} out:fly={{ duration: 500 }}>
+	<h1>
+		<div class="welcome">
+			<picture>
+				<source srcset="svelte-welcome.webp" type="image/webp" />
+				<img src="svelte-welcome.png" alt="Welcome" />
+			</picture>
+		</div>
+		{#if $userData.username}
+			<span style="text-transform: capitalize;">{$userData.username},</span>
+		{/if}
+
+		to our new<br />SvelteKit app
+	</h1>
+</section>
